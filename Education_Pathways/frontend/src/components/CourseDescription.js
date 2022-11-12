0e56173193fcd52e870e32b4f9771f74745f3c65
Priscilla Deng <priscilla.deng@mail.utoronto.ca>
2022-11-11 22:59:24 -0500

    Coreq/prereq/exclusions now have links to page

--- Education_Pathways/frontend/src/components/CourseDescription.js
+++ Education_Pathways/frontend/src/components/CourseDescription.js
@@ -25,15 +25,17 @@ class CourseDescriptionPage extends Component {
       course_term: "",
       activity: "",
       syllabus: "",
-      prerequisites: "",
-      corequisites: "",
-      exclusions: "",
+      // prerequisites: "",
+      // corequisites: "",
+      // exclusions: "",
+      excls: [],
+      prereqs: [],
+      coreqs: [],
       starred: false,
       graphics: [],
       username: localStorage.getItem('username')
     }
   }
-
 
 
   componentDidMount() {
@@ -52,9 +54,15 @@ class CourseDescriptionPage extends Component {
         this.setState({course_term: res.data.course.term})
         this.setState({graph: res.data.course.graph})
 
+        console.log("prereqs", this.state.prereqs)
+
         res.data.course.prereq = JSON.parse(res.data.course.prereq.replace(/'/g, '"'))
         res.data.course.coreq = JSON.parse(res.data.course.coreq.replace(/'/g, '"'))
         res.data.course.exclusion =JSON.parse(res.data.course.exclusion.replace(/'/g, '"'))
+
+        this.setState({prereqs: res.data.course.prereq})
+        this.setState({coreqs: res.data.course.coreq})
+        this.setState({excls: res.data.course.exclusion})
 
         let course_activity = res.data.course.activity
         course_activity=course_activity.replace("['", "")
@@ -72,52 +80,55 @@ class CourseDescriptionPage extends Component {
         this.setState({course_offering: course_activity})    
         this.setState({course_term: course_term})                     
 
-        let prereq_len = res.data.course.prereq.length
-        console.log("prereq:",res.data.course.prereq)
-        if (prereq_len > 1) {
-          let prereq_str = ""
-          for (let i = 0; i < prereq_len; i++) {
-            prereq_str += res.data.course.prereq[i]
-            if (i !== prereq_len - 1) {
-              prereq_str += ", "
-            }
-          }
-          this.setState({prerequisites : prereq_str})
-        } else {
-          this.setState({prerequisites : res.data.course.prereq})
-        }
+        // let prereq_len = res.data.course.prereq.length
+        // console.log("prereq:",res.data.course.prereq)
+        // if (prereq_len > 1) {
+        //   let prereq_str = ""
+        //   for (let i = 0; i < prereq_len; i++) {
+        //     prereq_str += res.data.course.prereq[i]
+        //     if (i !== prereq_len - 1) {
+        //       prereq_str += ", "
+        //     }
+        //   }
+        //   this.setState({prerequisites : prereq_str})
+        // } else {
+        //   this.setState({prerequisites : res.data.course.prereq})
+        // }
 
-        let coreq_len = res.data.course.coreq.length
-        console.log("coreq:",res.data.course.coreq)
-        if (coreq_len > 1) {
-          let coreq_str = ""
-          for (let i = 0; i < coreq_len; i++) {
-            coreq_str += res.data.course.coreq[i]
-            console.log("coreq_str:",coreq_str)
-            if (i !== coreq_len - 1) {
-              coreq_str += ", "
-            }
-          }
-          this.setState({corequisites : coreq_str})
-        } else {
-          this.setState({corequisites : res.data.course.coreq})
-        }
-
-        let exclusion_len = res.data.course.exclusion.length
-        console.log("exclusion:",res.data.course.exclusion)
-        if (exclusion_len > 1) {
-          let exclusion_str = ""
-          for (let i = 0; i < exclusion_len; i++) {
-            exclusion_str += res.data.course.exclusion[i]
-            console.log("exclusion_str:",exclusion_str)
-            if (i !== exclusion_len - 1) {
-              exclusion_str += ", "
-            }
-          }
-          this.setState({exclusions : exclusion_str})
-        } else {
-          this.setState({exclusions : res.data.course.exclusion})
-        }
+        // let coreq_len = res.data.course.coreq.length
+        // console.log("coreq:",res.data.course.coreq)
+        // if (coreq_len > 1) {
+        //   let coreq_str = ""
+        //   for (let i = 0; i < coreq_len; i++) {
+        //     coreq_str += res.data.course.coreq[i]
+        //     console.log("coreq_str:",coreq_str)
+        //     if (i !== coreq_len - 1) {
+        //       coreq_str += ", "
+        //     }
+        //   }
+        //   this.setState({corequisites : coreq_str})
+        // } else {
+        //   this.setState({corequisites : res.data.course.coreq})
+        // }
+        
+        // this.setState({exclusions_test: res.data.course.exclusion})
+        // let exclusion_len = res.data.course.exclusion.length
+        // console.log("exclusion:",res.data.course.exclusion)
+        // if (exclusion_len > 1) {
+        //   let exclusion_str = ""
+        //   for (let i = 0; i < exclusion_len; i++) {
+        //     exclusion_str += <a ref={`${res.data.course.exclusion[i]}`} onClick={this.redirectCourse}>{res.data.course.exclusion[i]}</a>
+        //     console.log("exclusion_str:",exclusion_str)
+        //     if (i !== exclusion_len - 1) {
+        //       exclusion_str += ", "
+        //     }
+        //   }
+        //   this.setState({exclusions : exclusion_str})
+        //   console.log("exc", this.state.exclusions_test)
+        // } else {
+        //   this.setState({exclusions : res.data.course.exclusion})
+        //   console.log("no exc")
+        // }
         let syllabus_link = "http://courses.skule.ca/course/" + this.props.code
         this.setState({syllabus : syllabus_link})
 
@@ -139,27 +150,6 @@ class CourseDescriptionPage extends Component {
   }
 
 	render() {
-    let coreq_holder
-    let prereq_holder
-    let exclusion_holder
-
-    if (this.state.exclusions == ''){
-      exclusion_holder = <p>None</p>
-    } else {
-      exclusion_holder = <p>{this.state.exclusions}</p>
-    }
-
-    if (this.state.prerequisites == ''){
-      prereq_holder = <p>None</p>
-    } else {
-      prereq_holder = <p>{this.state.prerequisites}</p>
-    }
-
-    if (this.state.corequisites == ''){
-      coreq_holder = <p>None</p>
-    } else {
-      coreq_holder = <p>{this.state.corequisites}</p>
-    }
     
 		return(
 
@@ -205,15 +195,25 @@ class CourseDescriptionPage extends Component {
             <Row>
               <Col className="requisites-display prerequisites">
                 <h4>Pre-Requisites</h4>
-                <p>{prereq_holder}</p>
+                <ul className="req-list">
+                  {this.state.prereqs.map(code => {
+                    return <a href={`/courseDetails/${code}`} onClick={this.redirectCourse}>{code}</a>
+                })}</ul>
               </Col>
               <Col className="requisites-display corequisites">
                 <h4>Co-Requisites</h4>
-                {coreq_holder}
+                <ul className="req-list">
+                  {this.state.coreqs.map(code => {
+                    return <a href={`/courseDetails/${code}`} onClick={this.redirectCourse}>{code}</a>
+                })}</ul>
+                
               </Col>
               <Col className="requisites-display exclusions">
                 <h4>Exclusions</h4>
-                <p>{exclusion_holder}</p>
+                <ul className="req-list">
+                  {this.state.excls.map(code => {
+                    return <a href={`/courseDetails/${code}`} onClick={this.redirectCourse}>{code}</a>
+                })}</ul>
               </Col>
             </Row>
             {/* <Row>

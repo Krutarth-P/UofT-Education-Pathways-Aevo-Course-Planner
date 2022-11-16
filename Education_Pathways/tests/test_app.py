2bc299d22314c572fd3dfd8b5b6c808e412f8867
Priscilla Deng <61913136+dengpris@users.noreply.github.com>
2022-11-16 15:52:00 -0500

    Add unit test for adding courses to CSV (#17)

--- Education_Pathways/tests/test_app.py
+++ Education_Pathways/tests/test_app.py
@@ -1,8 +1,31 @@
 from index import app
 from cgitb import reset
 import pytest
-from index import app
+from index import app, addNewCourse
 import json
+import csv
+
+# Unit test to ensure entries are being added to CSVs properly by Priscilla Deng
+@pytest.mark.parametrize("input", [{
+    "course_code":"ECE4444",
+    "course_name":"TESTinggggg",
+    "division":"TESTDIV",
+    "department":"",
+    "course_description":"BLEH",
+    "prerequisites":"",
+    "corequisites":"",
+    "exclusions":""}])
+
+def test_course_add_csv(input):
+    with open('resources/test_courses.csv', 'r+') as fp:
+        addNewCourse(input)
+        csvReader = list(csv.reader(fp, delimiter=","))
+        assert("ECE4444" in csvReader[-1][1])
+        assert("TESTinggggg" in csvReader[-1][2])
+        assert("TESTDIV" in csvReader[-1][3])
+        assert('BLEH' in csvReader[-1][4])
+        assert("test" not in csvReader[-1])
+    
 
 # Unit test function to test search bar functionalities by Jayce Wang
 @pytest.mark.parametrize("course_query", [

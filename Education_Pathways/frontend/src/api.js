0e0486b34a0101b67f1e557c0b650b3c2f6cb2c3
Priscilla <priscilla.deng@mail.utoronto.ca>
2022-11-16 22:53:19 -0500

    switch to heroku proxy

--- Education_Pathways/frontend/src/api.js
+++ Education_Pathways/frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  //baseURL: "https://ece444-group21-project1-aevo.herokuapp.com/"
-  baseURL: "http://localhost:5050/" 
+  baseURL: "https://ece444-group21-project1-aevo.herokuapp.com/"
+  // baseURL: "http://localhost:5050/" 
 });
